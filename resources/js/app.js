c9f5eaf71c01efae4b59205f0943c6c7a08cdab3
Shrey Singh <49588412+shreysingh8838@users.noreply.github.com>
2021-07-18 14:49:14 +0530

    Add files via upload

---
+++ resources/js/app.js
@@ -0,0 +1,111 @@
+import axios from 'axios'
+import Noty from 'noty'
+import { initAdmin } from './admin'
+import moment from 'moment'
+import { initStripe } from './stripe'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(item){
+    // also we can use fetch api 
+    // for ajax call by taking the data element from the client side we are sending it to the server
+    // by using famous library axios
+    axios.post('/update-cart',item).then(function (res){
+        console.log(res)
+        console.log(res.data.totalQty)
+        cartCounter.innerText = res.data.totalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false,
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false,
+        }).show();
+    })
+}
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        console.log(e);
+        let item = JSON.parse(btn.dataset.item)
+        console.log(item)
+
+        updateCart(item)
+    })
+})
+
+// Remove alert message after X seconds
+const alertMsg = document.querySelector('#success-alert')
+if(alertMsg) {
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000)
+}
+
+// Change order status
+let statuses = document.querySelectorAll('.status_line')
+let hiddenInput = document.querySelector('#hiddenInput')
+let order = hiddenInput ? hiddenInput.value : null
+order = JSON.parse(order)
+let time = document.createElement('small')
+
+function updateStatus(order) {
+   statuses.forEach((status) => {
+       status.classList.remove('step-completed')
+       status.classList.remove('current')
+   })
+   let stepCompleted = true;
+   statuses.forEach((status) => {
+      let dataProp = status.dataset.status
+      if(stepCompleted) {
+           status.classList.add('step-completed')
+      }
+      if(dataProp === order.status) {
+           stepCompleted = false
+           time.innerText = moment(order.updatedAt).format('hh:mm A')
+           status.appendChild(time)
+          if(status.nextElementSibling) {
+           status.nextElementSibling.classList.add('current')
+          }
+      }
+   })
+
+}
+
+updateStatus(order);
+
+initStripe()
+
+// Socket
+let socket = io()
+
+// Join
+if(order) {
+   socket.emit('join', `order_${order._id}`)
+}
+let adminAreaPath = window.location.pathname
+if(adminAreaPath.includes('admin')) {
+   initAdmin(socket)
+   socket.emit('join', 'adminRoom')
+}
+
+
+socket.on('orderUpdated', (data) => {
+   const updatedOrder = { ...order }
+   updatedOrder.updatedAt = moment().format()
+   updatedOrder.status = data.status
+   updateStatus(updatedOrder)
+   new Noty({
+       type: 'success',
+       timeout: 1000,
+       text: 'Order updated',
+       progressBar: false,
+   }).show();
+})
+
